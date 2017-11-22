87fa8a3a71a40061871fe06e5099c347513ab9a9
Kapeel <kapeelc@gmail.com>
2017-11-22 15:10:45 -0500

    added violin plot code

---
+++ Violin.R
@@ -0,0 +1,43 @@
+library(ggplot2)
+library(cowplot)
+setwd("/Users/kchougul/R_project/Violin_plots_OGE")
+ds = read.table("RLC.txt", sep="\t",header = TRUE)
+#ds$Specie <- factor(ds$Specie, levels = c('O. sativa vg. japonica', 'O. rufipogon', 'O. sativa vg. indica [93-11]', 'O. nivara', 'O. glaberrima', 'O. barthii', 'O. glumaepatula', 'O. meridionalis', 'O. punctata', 'O. brachyantha', 'L. perrieri'))
+
+#mylabels <- c('O. sativa vg. japonica',
+#              expression(paste("O. ",italic("rufipogon"))),
+#              'O. sativa vg. indica [93-11]',
+#              expression(paste("O. ",italic("nivara"))),
+#              expression(paste("O. ",italic("glaberrima"))),
+#              expression(paste("O. ",italic("barthii"))),
+#             expression(paste("O. ",italic("glumaepatula"))),
+#              expression(paste("O. ",italic("meridionalis"))),
+#              expression(paste("O. ",italic("punctata"))),
+#              expression(paste("O. ",italic("brachyantha"))),
+#              expression(paste("L. ",italic("perrieri")))
+#             ) 
+ds$Specie <- factor(ds$Specie, levels = c('O. sativa vg. japonica', 'O. rufipogon', 'O. sativa vg. indica [93-11]', 'O. nivara', 'O. glaberrima', 'O. barthii', 'O. glumaepatula', 'O. meridionalis', 'O. punctata', 'O. brachyantha', 'L. perrieri'))
+#ds$Specie <- factor(ds$Specie, levels = mylabels)
+
+# save plots
+savePlot <- function(myPlot) {
+  pdf("copia.pdf")
+  print(myPlot)
+  dev.off()
+}
+
+# Create a Violin plots
+myPlot <- ggplot(ds, aes(x = Specie, y =Insertion_age, fill="green")) + 
+  geom_violin() +
+  geom_boxplot(width=0.2) +
+  ylab('Insertion age (My)') + 
+  labs(title = "Copia") +
+  theme(text = element_text(size=12)) +
+  theme(axis.text.x=element_text(size=rel(0.8), angle=90)) +
+  theme(legend.position="none") +
+  scale_fill_manual(values=c("#FF3366"))
+  #scale_x_discrete(labels = mylabels)
+  # #99FF99 -> Gypsy
+  # FF3366 -> Copia
+  # 99CCFF > unknown
+savePlot(myPlot)
